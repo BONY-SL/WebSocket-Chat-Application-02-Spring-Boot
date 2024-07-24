3b8879681cbf7a864f31717c962bea8f3c1852ff
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-07-25 01:58:46 +0530

    WebShoket

---
+++ OneToOneChatApp/src/main/java/com/example/OneToOneChatApp/model/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.example.OneToOneChatApp.model;
+
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class ChatMessage {
+}
